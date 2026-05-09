5d969f8a386544700b6f703adf3e5151094a01bc
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-05-09 17:17:20 +0300

    First attempt

---
+++ home/modules/configs.nix
@@ -0,0 +1,23 @@
+{ config, ... }: {
+  xdg.configFile."niri" = {
+    source = ./niri;
+    recursive = true;
+  };
+
+  xdg.configFile."mako/config".source = ./mako/config;
+  xdg.configFile."foot/foot.ini".source = ./foot/foot.ini;
+  xdg.configFile."fastfetch/config.jsonc".source = ./fastfetch/config.jsonc;
+  xdg.configFile."wofi/config".source = ./wofi/config;
+  xdg.configFile."wofi/style.css".source = ./wofi/style.css;
+  xdg.configFile."waybar" = {
+    source = ./waybar;
+    recursive = true;
+  };
+
+  home.file.".vimrc".source = ./vim/vimrc;
+  home.file.".vim" = {
+    source = ./vim/dot-vim;
+    recursive = true;
+  };
+}
+
