bb4697f904360c6bbcaf1b9439c07b943ac6f718
Wilhelm <atlant@proton.me>
2026-05-09 17:17:20 +0300

    First attempt

---
+++ home/modules/gtk.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }: {
+  gtk = {
+    enable = true;
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+
+    theme = {
+      name = "Graphite-Dark";
+      package = pkgs.graphite-gtk-theme.override {
+        tweaks = [ "darker" "rimless" ]; 
+      };
+    };
+
+    iconTheme = {
+      name = "Tela-circle-black-dark";
+      package = pkgs.tela-circle-icon-theme.override {
+        colorVariants = [ "black" ];
+      };
+    };
+
+    font = {
+      name = "JetBrains Mono";
+      package = pkgs.jetbrains-mono;
+      size = 10;
+    };
+  };
+
+  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+}
