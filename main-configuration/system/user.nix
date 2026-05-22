93560ae5d8271e55ebe3a16ab701f677a9e0e314
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-05-22 17:39:21 +0300

    feat: make zsh main shell

--- main-configuration/system/user.nix
+++ main-configuration/system/user.nix
@@ -3,5 +3,17 @@
         isNormalUser = true;
         extraGroups = [ "wheel" ];
         packages = with pkgs; [];
+        shell = pkgs.zsh;
+    };
+
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" "docker" ];
+      };
     };
 }
