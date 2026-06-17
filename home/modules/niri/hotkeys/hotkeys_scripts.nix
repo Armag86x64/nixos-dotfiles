0cb6ada9de607e4496e95bdd43f1f801f9da3432
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-06-17 22:52:09 +0300

    feat(niri): add hotkey Shift+V for volume applet

--- home/modules/niri/hotkeys/hotkeys_scripts.nix
+++ home/modules/niri/hotkeys/hotkeys_scripts.nix
@@ -4,5 +4,8 @@
   programs.niri.settings.binds = {
     # Запуск графической утилиты для смены обоев
     "Mod+Shift+W".action.spawn = [ "waypaper" ];
+
+    # Виджеты
+    "Shift+V".action.spawn = [ "eww" "open" "--toggle" "bw-volume-applet" ];
   };
 }
