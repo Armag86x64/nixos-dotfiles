2a81149b07496f0c7cbb78100441484face36d84
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-08-21 20:10:02 +0300

    chore(altair-laptop): remove v2ray

--- main-configuration/altair-laptop/packages/networking.nix
+++ main-configuration/altair-laptop/packages/networking.nix
@@ -5,15 +5,17 @@
     tunMode.enable = true;
   };
 
+  /*
   services.v2raya = {
     enable = true;
     cliPackage = unstable.xray; 
   };
+  */
 
   environment.systemPackages = [
     unstable.localsend
 
-    unstable.v2ray-geoip
-    unstable.v2ray-domain-list-community
+    # unstable.v2ray-geoip
+    # unstable.v2ray-domain-list-community
   ];
 }
