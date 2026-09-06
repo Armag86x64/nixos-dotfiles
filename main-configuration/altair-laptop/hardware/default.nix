3c25510f542b658f964ad436b52507bfadb46c84
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-09-06 20:12:29 +0300

    feat(altair-laptop/external-hard: enable keyd and replace Copilot-button to SUPER

--- main-configuration/altair-laptop/hardware/default.nix
+++ main-configuration/altair-laptop/hardware/default.nix
@@ -2,6 +2,7 @@
   imports =
   [ 
     # ./hardware-configuration.nix
+    ./external-devs.nix
     ./bluetooth.nix
     ./audio.nix
     ./gpu.nix
---
+++ main-configuration/altair-laptop/hardware/external-devs.nix
@@ -0,0 +1,15 @@
+{ ... }: {
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            "leftshift+leftmeta+f23" = "leftmeta";
+          };
+        };
+      };
+    };
+  };
+}
