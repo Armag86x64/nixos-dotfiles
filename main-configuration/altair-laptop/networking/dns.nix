728597339f2ed2092a5f90142dacd7f64b5899d4
Armag86x64 <120749588+Armag86x64@users.noreply.github.com>
2026-09-16 18:21:28 +0300

    style(altair-laptop/netowork/dns): add comments

--- main-configuration/altair-laptop/networking/dns.nix
+++ main-configuration/altair-laptop/networking/dns.nix
@@ -1,6 +1,8 @@
 { ... }: {
+  # networking.networkmanager.insertNameservers = [ "8.8.8.8" "1.1.1.1" ];
   networking.nameservers = [
     "1.1.1.1" # Cloudflare
     "8.8.8.8" # Google
   ];
+  # networking.networkmanager.dns = "none";
 }
